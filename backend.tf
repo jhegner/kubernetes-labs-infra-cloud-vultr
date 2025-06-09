9eeb3328427745d5654d8524861fc94160f7edaf
Jidlafe Hegner <jidlafehegner@gmail.com>
2025-06-08 23:52:04 -0300

    fix: nao permitido uso de variavel

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "kubernetes-labs-tf-backend-aws-s3bucket"
     key            = "lab/state/terraform"
-    region         = local.AWS_REGION
+    region         = "us-east-1"
     use_lockfile   = true
   }
 }
