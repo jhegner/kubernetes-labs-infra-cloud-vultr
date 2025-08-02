c57c4b9177e5d9324ed862a4b4f944555e795ae7
Jidlafe Hegner <jidlafehegner@gmail.com>
2025-08-01 22:01:27 -0300

    chore: arquivo executavel sh

--- scripts/aws/0-s3-delete-statefile.sh
+++ scripts/aws/0-s3-delete-statefile.sh
@@ -5,3 +5,4 @@ cd ../../
 echo "--- Removendo o arquivo terraform de estado da infra vultr ---"
 
 aws s3 rm s3://kubernetes-labs-tf-backend-aws-s3bucket/lab/state/terraform-vultr-infra --recursive
+aws s3 rm s3://kubernetes-labs-tf-backend-aws-s3bucket/lab/state/terraform-vultr-infra.tflock
