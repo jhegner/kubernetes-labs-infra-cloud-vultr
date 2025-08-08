beb3de41bd66d14bfeb186980154d892032c9a44
jhegner <jidlafehegner@gmail.com>
2025-08-08 01:29:37 -0300

    chore: habilitado criacao cr image

--- toggle.tf
+++ toggle.tf
@@ -1,7 +1,7 @@
 # Define toggle variables for enabling or disabling modules creation
 locals {
   toggle_modules = {
-    create_container_registry = false
+    create_container_registry = true
     create_vpc_network        = true
     create_firewall_rules     = true
     create_databases          = false
